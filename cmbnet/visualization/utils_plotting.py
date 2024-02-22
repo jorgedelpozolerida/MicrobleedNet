9bc5c6c68cde4adb7c41404114da5639aca62fd1
jorgedelpozolerida <jorgedelpozolerida@gmail.com>
2024-02-22 01:46:41 +0100

    better visualization

--- cmbnet/visualization/utils_plotting.py
+++ cmbnet/visualization/utils_plotting.py
@@ -149,6 +149,122 @@ def plot_processed_mask(mri_im: Any, cmb_im: Any, processed_cmb_im: Any, cmb_coo
     plt.close(fig)
 
 
+def plot_processed_mask_2x3(mri_im: Any, cmb_im: Any, processed_cmb_im: Any, cmb_coords: Tuple[float, float, float], zoom_size: float, metadata_str: str = None, save_path: str = None) -> None:
+    """
+    Plot MRI, CMB, and processed CMB images in a 2x3 subplot layout, highlighting the specified CMB location across axial, sagittal, and coronal views.
+
+    Args:
+        mri_im: NiBabel object containing MRI image data.
+        cmb_im: NiBabel object containing original CMB mask data.
+        processed_cmb_im: NiBabel object containing processed CMB mask data.
+        cmb_coords: Tuple specifying the coordinates (x, y, z) of the CMB.
+        zoom_size: The diameter of the zoom window centered on the CMB coordinates.
+        metadata_str: Optional string containing metadata to be displayed.
+        save_path: Optional path to save the generated plot image.
+    """
+    half_size = zoom_size / 2
+
+    # Verify orientation consistency across images
+    if not check_orientation_consistency([mri_im.affine, cmb_im.affine, processed_cmb_im.affine]):
+        raise ValueError("All images must have the same orientation.")
+
+    # Extracting data and handling 4D images by selecting the first volume
+    mri_data = np.squeeze(mri_im.get_fdata())
+    cmb_data = np.squeeze(cmb_im.get_fdata())
+    out_mask = np.squeeze(processed_cmb_im.get_fdata())
+
+    # Define cropping slices for axial, sagittal, and coronal views
+    axial_crop = [slice(max(0, int(cmb_coords[i] - half_size)), min(dim, int(cmb_coords[i] + half_size))) for i, dim in enumerate(mri_data.shape)]
+    sagittal_crop = axial_crop.copy()
+    coronal_crop = axial_crop.copy()
+
+    # Adjust for full range in non-cropped dimensions
+    coronal_crop[1] = slice(None) 
+    sagittal_crop[0] = slice(None)  
+    axial_crop[2] = slice(None)  # Ensure full range for z in axial
+
+    # Convert slices to tuples for indexing
+    sagittal_crop = tuple(sagittal_crop)
+    coronal_crop = tuple(coronal_crop)
+    axial_crop = tuple(axial_crop)
+
+    # Cropping data for each view
+    t2s_cropped_dict = {
+        'axial': mri_data[axial_crop],
+        'sagittal': mri_data[sagittal_crop],
+        'coronal': mri_data[coronal_crop],
+    }
+    cmb_cropped_dict = {
+        'axial': cmb_data[axial_crop],
+        'sagittal': cmb_data[sagittal_crop],
+        'coronal': cmb_data[coronal_crop],
+    }
+    processed_cropped_dict = {
+        'axial': out_mask[axial_crop],
+        'sagittal': out_mask[sagittal_crop],
+        'coronal': out_mask[coronal_crop],
+    }
+
+    # Initialize figure and axes for 2x3 layout
+    fig, axs = plt.subplots(2, 3, figsize=(15, 10))
+
+    # Plot the full view without zoom
+    axs[0, 0].imshow(mri_data[:, :, cmb_coords[-1]], cmap='gray')
+    plot_cross_with_gap(axs[0, 0], y=cmb_coords[0], x=cmb_coords[1], color='yellow', line_length=25, gap_size=15, linewidth=1)
+
+    # Plot the full view with zoom on axial
+    axs[0, 1].imshow(t2s_cropped_dict['axial'][:, :, cmb_coords[-1]], cmap='gray')
+    plot_cross_with_gap(axs[0, 1], y=t2s_cropped_dict['axial'].shape[0] / 2, x=t2s_cropped_dict['axial'].shape[1] / 2, color='yellow', line_length=25, gap_size=15, linewidth=1)
+
+    # Plot original and generated masks with cross on axial
+    axs[0, 2].imshow(t2s_cropped_dict['axial'][:, :, cmb_coords[-1]], cmap='gray')
+    axs[0, 2].imshow(cmb_cropped_dict['axial'][:, :, cmb_coords[-1]], alpha=0.5, cmap="Reds")
+    plot_cross_with_gap(axs[0, 2], y=t2s_cropped_dict['axial'].shape[0] / 2, x=t2s_cropped_dict['axial'].shape[1] / 2, color='yellow', line_length=25, gap_size=15, linewidth=1)
+
+    # Plotting cropped views for axial, sagittal, and coronal with processed masks
+    for i, view in enumerate(['axial', 'sagittal', 'coronal']):
+        img_slice, mask_slice = None, None
+        if view == 'axial':
+            ind = 2
+            slice_index = cmb_coords[ind]  # Z-axis for axial
+            img_slice = t2s_cropped_dict[view][:, :, slice_index]
+            mask_slice = processed_cropped_dict[view][:, :, slice_index]
+            y_cross = t2s_cropped_dict[view].shape[0] / 2
+            x_cross = t2s_cropped_dict[view].shape[1] / 2
+        elif view == 'sagittal':
+            ind = 0
+            slice_index = cmb_coords[ind]  # X-axis for sagittal
+            img_slice = t2s_cropped_dict[view][slice_index, :, :]
+            mask_slice = processed_cropped_dict[view][slice_index, :, :]
+            y_cross = t2s_cropped_dict[view].shape[1] / 2
+            x_cross = t2s_cropped_dict[view].shape[2] / 2
+        elif view == 'coronal':
+            ind = 1
+            slice_index = cmb_coords[ind]  # Y-axis for coronal
+            img_slice = t2s_cropped_dict[view][:, slice_index, :]
+            mask_slice = processed_cropped_dict[view][:, slice_index, :]
+            y_cross = t2s_cropped_dict[view].shape[0] / 2
+            x_cross = t2s_cropped_dict[view].shape[2] / 2
+        
+        axs[1, i].imshow(img_slice, cmap='gray')
+        axs[1, i].imshow(mask_slice, alpha=0.5, cmap="Reds")
+
+        # plot_cross_with_gap(axs[1, i], y=y_cross, x=x_cross, color='yellow', line_length=25, gap_size=15, linewidth=1)
+
+    # Optionally add metadata
+    if metadata_str:
+        fig.suptitle(
+            metadata_str
+        )
+    # Hide axes and show layout
+    for ax in axs.ravel():
+        ax.axis('off')
+    plt.tight_layout()
+    plt.savefig(save_path)
+    plt.close(fig)
+
+
+
 def generate_cmb_plots(subject, mri_im, raw_cmb, processed_cmb, cmb_metadata, plots_path, zoom_size=100):
     """
     Saves plots for zoomed cerebral microbleeds
@@ -169,24 +285,18 @@ def generate_cmb_plots(subject, mri_im, raw_cmb, processed_cmb, cmb_metadata, pl
 
         # Start generating the metadata string
         metadata_str_parts = [
-            f"Image shape: {mri_im.shape}, zoom_size={zoom_size}",
-            f"CMB ---- CM={CM},  size={met_i['size']},  radius={met_i['radius']}",
+            f"Im. shape: {mri_im.shape}, zoom_size={zoom_size}",
+            f"CM={CM},  size={met_i['size']},  radius={met_i['radius']}",
         ]
         # Add optional parts only if they are present
         if region_growing := met_i.get('region_growing', {}):
-            optional_keys = ['connectivity', 'intensity_mode', 'diff_mode', 'distance_th', 'size_th']
+            optional_keys = ['distance_th', 'size_th', 'connectivity', 'intensity_mode', 'diff_mode']
             optional_parts = [
-                f'"{key}": {region_growing[key]}' for key in optional_keys if key in region_growing
+                f'"{region_growing[key]}"' for key in optional_keys if key in region_growing
             ]
 
-            # Group the optional parts two by two and join each pair with a newline
-            # If the number of optional parts is odd, the last part will stand alone without a newline at the end
-            metadata_str_parts.extend(
-                "\n".join(optional_parts[i : i + 2])
-                for i in range(0, len(optional_parts), 2)
-            )
-        # Join all parts of the metadata string
-        metadata_str = "\n".join(metadata_str_parts)
-
+            metadata_str_parts.extend(optional_parts)
+        # Join all parts of the metadata string with a comma and a space for a single-line string
+        metadata_str = ", ".join(metadata_str_parts)
         # Plot and save the data
-        plot_processed_mask(mri_im, raw_cmb, processed_cmb, CM, zoom_size, metadata_str=metadata_str, save_path=filename_temp)
+        plot_processed_mask_2x3(mri_im, raw_cmb, processed_cmb, CM, zoom_size, metadata_str=metadata_str, save_path=filename_temp)
